1cc6c5919da5ddb5f28f099df0f9fc6c4b962fc2
Nitesh Turaga <nitesh.turaga@tempus.com>
2022-10-13 16:01:22 -0400

    Update render_different.R

--- render_different.R
+++ render_different.R
@@ -1,7 +1,28 @@
+library(rmarkdown)
+library(lubridate)
 
-renderMyDocument <- function(region, start) {
-    rmarkdown::render("MyDocument.Rmd", params = list(
-        region = region,
-        start = start
-    ))
+## Pass in other params for render() as required 
+renderMyDocument <- function(template_doc = "demo.Rmd",
+                             genes, cancers,
+                             output_dir = ".",
+                             output_file, ...) {
+    output_file <- paste0(output_file, "-", lubridate::today(),".html")
+    
+    res <- rmarkdown::render(template_doc,
+                             params = list(
+                                 gene1 = genes[1],
+                                 gene2 = genes[2],
+                                 cancer_types_vector = cancers),
+                             output_dir = output_dir,
+                             output_file = output_file,                  
+                             ...)
+    res
 }
+
+## Call function
+
+renderMyDocument(template_doc = "demo.Rmd",
+                 genes = c('BRCA1', 'BRCA2'),
+                 cancers = c('Breast', 'Prostate', 'Bone'),
+                 output_dir = ".",
+                 output_file = 'test')
